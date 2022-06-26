f4e3c06cb605e7809c890c07ef9fb855119858ad
Emem Udofia <ememudofia456@gmail.com>
2022-06-26 05:12:52 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	char text[9] = "_putchar";
-	int t = 0;
+        char text[9] = "_putchar";
+        int t = 0;
 
-	for (t = 0; t < 8; t++)
-	{
-		_putchar(text[t]);
-	}
-	_putchar('\n');
-	return (0);
+        for (t = 0; t < 8; t++)
+        {
+                _putchar(text[t]);
+        }
+        _putchar('\n');
+        return (0);
 }
